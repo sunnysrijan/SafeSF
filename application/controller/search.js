981626b7af96ec8802faa26b253a960f10e4e2df
ryanstills <stillwagonryan@gmail.com>
2019-07-15 19:34:53 -0700

    abstracted search query builder into its own file called db_query.

--- application/controller/search.js
+++ application/controller/search.js
@@ -5,69 +5,28 @@
 
 
 const db = require('../auth/db_config.js')
+const db_query = require('../database/db_query.js')
 
 exports.getResults = function(data, callback) {
 
     console.log(data)
 
     //sets up the basics for the query 
-    var search_query = 'SELECT * FROM reports'
-    var where_clause = ' WHERE'
-    var order_clause = ' ORDER BY report_insert_date DESC'
-    var where_count = 0
-    var values = []
-    
-    //checks if any parameters have been sent
-    //if not it grabs the whole list
-    if(data) {
-        
-        //for each parameter sent in it adds a condition to the where clause
-        //values are pushed into the values array
-        for(var [key, value] of Object.entries(data))
-        {
-            if(where_count > 0)
-            {
-                where_clause = where_clause + " AND "
-            }
-            //user entry is a special case
-            //this parameter must have a % LIKE wrapped around it
-            if(key === 'user_entry')
-            {
-                where_clause += " report_details LIKE ?"
-                where_count++
-                value = "%" + value + "%"
-                values.push(value)
-            }
-            else
-            {
-                where_clause = where_clause + " " + key + " = ?"
-                where_count++
-                values.push(value)
-            }
-        }
-        if(where_count > 0)
-        {
-            search_query = search_query + where_clause + order_clause
-        }
-        else
-        {
-            search_query = search_query + order_clause
-        }
-    }
+    var search_table = 'reports'
+    var order_by_field = 'report_insert_date'
 
-    //output for debugging purposes on the server
-    console.log(search_query)
-
-    //values array is passed in to bind the parameter values to the query
-    //this projects against sql injections and is necessary
-    db.query(search_query, values, function(err, result) {
-        if(err) {
-            console.log("Error querying database : " + err)
-            callback(err, null)
-        }
-        else {
-            console.log("Results succesfully retrieved")
-            callback(null, result)
-        }
+    db_query.buildSearchQuery(search_table, order_by_field, data).then(function(query){
+        db.query(query[0], query[1], function(err, result) {
+            console.log(query[0])
+            console.log(query[1])
+            if(err) {
+                console.log("Error querying database : " + err)
+                callback(err, null)
+            }
+            else {
+                console.log("Results succesfully retrieved")
+                callback(null, result)
+            }
+        })
     })
 }
