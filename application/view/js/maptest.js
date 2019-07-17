3da803546be02661456f97b42ec8090ad1876d46
JammyPajamies <JammyPajamies@users.noreply.github.com>
2019-07-16 21:41:01 -0700

    Conversion from using lat and lng seperately to a single two-var tuple.

--- application/view/js/maptest.js
+++ application/view/js/maptest.js
@@ -1,30 +1,33 @@
+/*
+Written by: Evan Guan
+Course: CSc 648 Software Engineering Summer 2019 Team 2
+*/
 
-var curLat = Number(37.783);
-var curLng = Number(-122.416);
+var curLatLng = {lat: 37.783, lng: -122.416}
 var map;
 var marker;
 
 function getCurLat() {
-  return Number(curLat);
+  return Number(curLatLng.lat);
 }
 
 function getCurLng() {
-  return Number(curLng);
+  return Number(curLatLng.lng);
 }
 
 function setCurLat(newLat) {
-  curLat = Number(newLat);
+  curLatLng.lat = Number(newLat);
 }
 
 function setCurLng(newLng) {
-  curLng = Number(newLng);
+  curLatLng.lng = Number(newLng);
 }
 
 /* Function locates the map div and fills it with the map. */
 function initMap() {
   /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: getCurLat(), lng: getCurLng()},
+    center: curLatLng,
     zoom: 12.1
   });
   /* Then a marker is placed at the lat/lng. */
@@ -32,20 +35,22 @@ function initMap() {
   console.log('Map loading finished.')
 }
 
+/* Essentially the same as initializing the map, but with a different zoom level. */
+function updateMap() {
+  /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: curLatLng,
+    zoom: 20
+  });
+  /* Then a marker is placed at the lat/lng. */
+  marker = new google.maps.Marker({position: {lat: getCurLat(), lng: getCurLng()}, map: map});
+  console.log('Map updated.')
+}
+
 function setNewLatLng(newLat, newLng)
 {
   setCurLat(37.787993);
   setCurLng(-122.505875);
-}
-
-function updateMap() {
-  /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: getCurLat(), lng: getCurLng()},
-    zoom: 12.1
-  });
-  /* Then a marker is placed at the lat/lng. */
-  marker = new google.maps.Marker({position: {lat: getCurLat(), lng: getCurLng()}, map: map});
 }
 
 console.log(getCurLat(), getCurLng());
