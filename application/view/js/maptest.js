17bd3f568ef787f451a96e017f58b6423c9ca9ad
JammyPajamies <JammyPajamies@users.noreply.github.com>
2019-07-16 21:52:58 -0700

    Cleaned up comments, map changes to hybrid viewpoint in order to emphasize map update.

--- application/view/js/maptest.js
+++ application/view/js/maptest.js
@@ -23,34 +23,35 @@ function setCurLng(newLng) {
   curLatLng.lng = Number(newLng);
 }
 
+function setNewLatLng(newLat, newLng) {
+  setCurLat(37.787993);
+  setCurLng(-122.505875);
+}
+
 /* Function locates the map div and fills it with the map. */
 function initMap() {
   /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
   map = new google.maps.Map(document.getElementById('map'), {
     center: curLatLng,
-    zoom: 12.1
+    zoom: 12.1,
+    mapTypeId: 'roadmap'
   });
   /* Then a marker is placed at the lat/lng. */
   marker = new google.maps.Marker({position: {lat: getCurLat(), lng: getCurLng()}, map: map});
   console.log('Map loading finished.')
 }
 
-/* Essentially the same as initializing the map, but with a different zoom level. */
+/* Essentially the same as initializing the map, but with a different zoom level and hybrid view. */
 function updateMap() {
   /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
   map = new google.maps.Map(document.getElementById('map'), {
     center: curLatLng,
-    zoom: 20
+    zoom: 20,
+    mapTypeId: 'hybrid'
   });
   /* Then a marker is placed at the lat/lng. */
   marker = new google.maps.Marker({position: {lat: getCurLat(), lng: getCurLng()}, map: map});
   console.log('Map updated.')
-}
-
-function setNewLatLng(newLat, newLng)
-{
-  setCurLat(37.787993);
-  setCurLng(-122.505875);
 }
 
 console.log(getCurLat(), getCurLng());
