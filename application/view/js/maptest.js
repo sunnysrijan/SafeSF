56bffd71a6ad03f5525d46ba368012db13010a5d
JammyPajamies <JammyPajamies@users.noreply.github.com>
2019-07-18 17:54:40 -0700

    Geocoding api in JS function teesting, multiple markers testing.
    - Added function to convert from a string location name to a coordinate that can be used by the map api.
    - Tested adding multiple markers to the map. (Test success)
    - Tested using different marker icons. (Test success)

--- application/view/js/maptest.js
+++ application/view/js/maptest.js
@@ -3,9 +3,14 @@ Written by: Evan Guan
 Course: CSc 648 Software Engineering Summer 2019 Team 2
 */
 
+// The current saved coordinates of the center of the map.
 var curLatLng = {lat: 37.783, lng: -122.416}
+// The map object itself.
 var map;
+// A list of marker objects.
 var marker;
+// The geocoder object.
+var geocoder;
 
 function getCurLat() {
   return Number(curLatLng.lat);
@@ -24,8 +29,8 @@ function setCurLng(newLng) {
 }
 
 function setNewLatLng(newLat, newLng) {
-  setCurLat(37.787993);
-  setCurLng(-122.505875);
+  setCurLat(Number(newLat));
+  setCurLng(Number(newLng));
 }
 
 /* Function locates the map div and fills it with the map. */
@@ -36,9 +41,17 @@ function initMap() {
     zoom: 12.1,
     mapTypeId: 'roadmap'
   });
+
+  /* We can initialize the geocoder after we have a map */
+  this.geocoder = new google.maps.Geocoder();
+
   /* Then a marker is placed at the lat/lng. */
   marker = new google.maps.Marker({position: {lat: getCurLat(), lng: getCurLng()}, map: map});
-  console.log('Map loading finished.')
+  console.log('Marker 1 added.')
+  marker = new google.maps.Marker({position: {lat: getCurLat() + .001, lng: getCurLng() + .001}, map: this.map});
+  console.log('Marker 2 added.')
+  marker = new google.maps.Marker({position: {lat: getCurLat() + .002, lng: getCurLng() + .002}, map: map});
+  console.log('Marker 3 added.')
 }
 
 /* Essentially the same as initializing the map, but with a different zoom level and hybrid view. */
@@ -46,12 +59,59 @@ function updateMap() {
   /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
   map = new google.maps.Map(document.getElementById('map'), {
     center: curLatLng,
-    zoom: 20,
+    zoom: 18,
     mapTypeId: 'hybrid'
   });
   /* Then a marker is placed at the lat/lng. */
-  marker = new google.maps.Marker({position: {lat: getCurLat(), lng: getCurLng()}, map: map});
+  marker = new google.maps.Marker({
+    position: {lat: getCurLat(), lng: getCurLng()},
+    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+    map: map
+  });
   console.log('Map updated.')
+  addMarkerToMap('Mile Rock Beach, San Francisco');
 }
 
-console.log(getCurLat(), getCurLng());
+/* Adds a marker to the map, given coordinates or place name. */
+function addMarkerToMap(newMarkerCoords) {
+  /* Check to see if the var we get is a string.
+  If it is, try to convert it to a coordinate. */
+  if(typeof(newMarkerCoords) === 'string') {
+    newMarkerCoords = geocodeAddress(newMarkerCoords);
+  }
+  /* Now, create the new marker. */
+  marker = new google.maps.Marker({
+    position: newMarkerCoords,
+    icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    map: this.map
+  });
+
+  /* Testing using coordinates in a for loop. TEST OUTPUT OK. */
+  /*
+  for (i = 0; i < 5; i++)
+  {
+    var curCoord = this.curLatLong;
+    marker = new google.maps.Marker({
+      position: {lat: getCurLat() + (i * 0.0001), lng: getCurLng() + (i * 0.0001)},
+      map: map
+    });
+  }
+  */
+}
+
+/* Uses Google's geocoding service to return the coordinates of a string placename. */
+function geocodeAddress(locationNameString) {
+  var locationName = String(locationNameString);
+  geocoder.geocode({'address': locationName}, function(results, status) {
+    if (status === 'OK') {
+      // this.map.setCenter(results[0].geometry.location); // Recenter the map on the new marker.
+      this.marker = new google.maps.Marker({
+        position: results[0].geometry.location,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        map: this.map,
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
