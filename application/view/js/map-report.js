b37739e22ef72556ec52cff88b3ac762d73f4465
JammyPajamies <JammyPajamies@users.noreply.github.com>
2019-07-24 04:24:18 -0700

    Changed comment in initMap function to mitigate against potential misunderstanding.

--- application/view/js/map-report.js
+++ application/view/js/map-report.js
@@ -1,9 +1,10 @@
 var curLatLng = {lat: 37.7749, lng: -122.4194}
 
 function initMap() {
-  /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
+  // Initiate map centered on SF by default.
   map = new google.maps.Map(document.getElementById('map'), {
     center: curLatLng,
     zoom: 12,
     mapTypeId: 'hybrid'
-  });}
+  });
+}
