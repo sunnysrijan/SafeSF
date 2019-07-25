fcc0955ec97f914285b26cc601fc13f3e2b15d7b
JammyPajamies <JammyPajamies@users.noreply.github.com>
2019-07-24 19:02:26 -0700

    Further geocoding trimming and fixing of demo. Also added comments with code to expedite integration.

--- application/view/js/HomePageMapExample.js
+++ application/view/js/HomePageMapExample.js
@@ -24,11 +24,16 @@ function initMap() {
     zoom: 12,
     mapTypeId: 'hybrid'
   });
+
+  setNewCenterLatLng({lat: 37.720993, lng: -122.475373});
+
+  addExistingReportMarkersToMap();
 }
 
 /*
 Adds a marker to the map, given coordinates or place name.
-Will potentially change to use a foreach internally to post all relevant markers.
+Modify the parameters to send this function data to use:
+function addExistingReportMarkersToMap(reportTitle, reportHazardType, reportThumbnail, reportSummary, reportID, markerCoords, markerIconURL)
 */
 function addExistingReportMarkersToMap() {
   // This data will be collected from the json returned by a DB query.
@@ -38,21 +43,31 @@ function addExistingReportMarkersToMap() {
   var infoWindowSummary;        // The brief/truncated summary of the report from the DB query json.
   var infoWindowReportURL;      // The full url of the report.
                                 // Likely will just be the report id appended to boilerplate url.
-  var infoWindowContentString;  // The string of html that will be in the popup for the marker on-click.
-  var markerIcon;               // The url of the image to use for the marker icon.
+  var markerCoords;             // The hash {lat: float, lng:float} for the coordinates. See var curLatLng above for formatting.
+  var markerIconURL;            // The url of the image to use for the marker icon.
                                 // Icons from Google: https://sites.google.com/site/gmapsdevelopment/
 
-  /* TODO:  Create functions to read json return from the DB and parse into the relevant vars.
-            Do this before attempting to parse the coordinates, as that is an asyncronous api call.
-  */
+  var infoWindowContentString;  // The string of html that will be in the popup for the marker on-click.
 
   // Hard coded example.
   infoWindowTitle = 'Generic Title Here';
-  infoWindowHazardType = 'Oil Hazard Alert: ';
+  infoWindowHazardType = 'Oil Hazard Alert';
   infoWindowThumbnail = 'images/example-infowindow-thumbnail.png';
   infoWindowSummary = 'Large oil slick on intersection of Holloway and 19th avenue.';
   infoWindowReportURL = 'https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple';
-  markerIcon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+  markerCoords = curLatLng;
+  markerIconURL = 'http://maps.google.com/mapfiles/ms/micons/caution.png';
+
+  // Dynamic code
+  /*
+  infoWindowTitle = reportTitle;
+  infoWindowHazardType = reportHazardType + ' Alert';
+  infoWindowThumbnail = reportThumbnail;
+  infoWindowSummary = reportSummary;
+  infoWindowReportURL = 'https://our.site.ip/reports/' + reportID;
+  markerCoords = markerCoords;
+  markerIconURL = markerIconURL;
+  */
 
   // Create the content string. It is essentially pure html.
   infoWindowContentString =
@@ -83,35 +98,15 @@ function addExistingReportMarkersToMap() {
           content: infoWindowContentString
         });
 
-  /* Check to see if the var we get is a string.
-  If it is, try to convert it to a coordinate. */
-  if(typeof(newMarkerCoords) === 'string') {
-    geocodePlaceNameToLatLng(newMarkerCoords).then(function(resultCoords) {
-      console.log('resultCoords: ', resultCoords);
-      /* Now, create the new marker. */
-      marker = new google.maps.Marker({
-        position: resultCoords,
-        icon: markerIcon,
-        title: infoWindowHazardType,
-        map: this.map
-      });
+  // Now, create the new marker.
+  marker = new google.maps.Marker({
+    position: markerCoords,
+    icon: markerIconURL,
+    title: infoWindowHazardType,
+    map: this.map
+  });
 
-      marker.addListener('click', function() {
-        infowindow.open(map, marker);
-      });
-    });
-  } else {
-    console.log('resultCoords: ', resultCoords);
-    // Now, create the new marker.
-    marker = new google.maps.Marker({
-      position: resultCoords,
-      icon: markerIcon,
-      title: infoWindowHazardType,
-      map: this.map
-    });
-
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-  }
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 }
