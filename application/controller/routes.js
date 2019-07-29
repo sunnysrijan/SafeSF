9c6e6b23ddc5713406f6aacbbfba4b693050c314
Alex Wolski <awolski@mail.sfsu.edu>
2019-07-29 16:45:23 -0700

    Removed team pages and routes

--- application/controller/routes.js
+++ application/controller/routes.js
@@ -14,43 +14,6 @@ router.use(bodyParser.urlencoded({
 }))
 router.use(bodyParser.json())
 
-
-
-router.get('/davids', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/davids.html'))
-})
-
-router.get('/harshs', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/harshs.html'))
-})
-
-router.get('/tristans', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/tristans.html'))
-})
-
-router.get('/lidiyas', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/lidiyas.html'))
-})
-
-router.get('/sunnys', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/sunnys.html'))
-})
-
-router.get('/alexs', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/alexs.html'))
-})
-
-router.get('/evans', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/evans.html'))
-})
-
 router.get('/search', (req, res) => {
     db_search.getResults(req.query, function(err, result) {
         console.log(req.query)
@@ -174,15 +137,9 @@ router.get('/requestAccess', (req, res) => {
     })
 })
 
-router.get('/team', (req, res) => {
-    res.status(200)
-    res.sendFile(path.resolve('view/team.html'))
-})
-
 router.get('/about', (req, res) => {
     res.status(200)
     res.sendFile(path.resolve('view/about.html'))
 })
-
 
 module.exports = router
