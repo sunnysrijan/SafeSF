ca9777a53f8f3c7fe82088c3d7952573267056c9
JammyPajamies <JammyPajamies@users.noreply.github.com>
2019-07-31 21:08:39 -0700

    Added marker functionality to homepage. Zooms in to marker on click, zooms back out on popup closing.

--- application/view/js/search.js
+++ application/view/js/search.js
@@ -1,182 +1,316 @@
 var categories;
 var locations;
+// The current saved coordinates of the center of the map.
+var curLatLng = {
+  lat: 37.7749,
+  lng: -122.4194
+}
+// The map object itself.
+var map;
+// An array of marker objects.
+var markers = [];
 
 function getCategories() {
-    var xmlReq = new XMLHttpRequest();
+  var xmlReq = new XMLHttpRequest();
 
-    xmlReq.onload = function() {
-        if (xmlReq.status == 200) {
-            categories = xmlReq.response
-            populateDropdown(categories, "category");
-        }
+  xmlReq.onload = function() {
+    if (xmlReq.status == 200) {
+      categories = xmlReq.response
+      populateDropdown(categories, "category");
     }
+  }
 
-    xmlReq.open('GET', '/categories', true);
-    xmlReq.responseType = "json";
-    xmlReq.send();
+  xmlReq.open('GET', '/categories', true);
+  xmlReq.responseType = "json";
+  xmlReq.send();
 }
 
 function getLocations() {
-    var xmlReq = new XMLHttpRequest();
+  var xmlReq = new XMLHttpRequest();
 
-    xmlReq.onload = function() {
-        if (xmlReq.status == 200) {
-            locations = xmlReq.response
-            populateDropdown(locations, "location");
-        }
+  xmlReq.onload = function() {
+    if (xmlReq.status == 200) {
+      locations = xmlReq.response
+      populateDropdown(locations, "location");
     }
+  }
 
-    xmlReq.open('GET', '/locations', true);
-    xmlReq.responseType = "json";
-    xmlReq.send();
+  xmlReq.open('GET', '/locations', true);
+  xmlReq.responseType = "json";
+  xmlReq.send();
 }
 
 function populateDropdown(jsonData, field) {
-    if(field === "category")
-        var dropDown = document.getElementById("categoryDropDown");
-    else if(field === "location")
-        var dropDown = document.getElementById("locationDropDown");
+  if (field === "category")
+    var dropDown = document.getElementById("categoryDropDown");
+  else if (field === "location")
+    var dropDown = document.getElementById("locationDropDown");
 
-    var div = document.createElement("div");
-    div.innerHTML = "<a href='javascript:void(0);' onclick=\"selectDropdown('None', '" + field + "')\">None</a>";
+  var div = document.createElement("div");
+  div.innerHTML = "<a href='javascript:void(0);' onclick=\"selectDropdown('None', '" + field + "')\">None</a>";
+  dropDown.appendChild(div);
+
+  for (var i = 0; i < jsonData.length; i++) {
+    if (field === "category")
+      var selection = jsonData[i].category;
+    else if (field === "location")
+      var selection = jsonData[i].location;
+
+    div = document.createElement("div");
+    div.innerHTML = "<a href='javascript:void(0);' onclick=\"selectDropdown('" + selection + "', '" + field + "')\">" + selection + "</a>";
     dropDown.appendChild(div);
-
-    for(var i = 0; i < jsonData.length; i++) {
-        if(field === "category")
-            var selection = jsonData[i].category;
-        else if(field === "location")
-            var selection = jsonData[i].location;
-
-        div = document.createElement("div");
-        div.innerHTML = "<a href='javascript:void(0);' onclick=\"selectDropdown('" + selection + "', '" + field + "')\">" + selection + "</a>";
-        dropDown.appendChild(div);
-    }
+  }
 }
 
 function selectDropdown(selection, field) {
-    if(field === "category")
-        var dropDownButton = "categoryButton";
-    else if(field === "location")
-        var dropDownButton = "locationButton";
+  if (field === "category")
+    var dropDownButton = "categoryButton";
+  else if (field === "location")
+    var dropDownButton = "locationButton";
 
-    var button = document.getElementById(dropDownButton);
+  var button = document.getElementById(dropDownButton);
 
-    if(selection === "None") {
-        if(field === "category")
-            button.innerHTML = "Categories";
-        else if(field === "location")
-            button.innerHTML = "Locations";
-    }
-    else
-        button.innerHTML = selection;
+  if (selection === "None") {
+    if (field === "category")
+      button.innerHTML = "Categories";
+    else if (field === "location")
+      button.innerHTML = "Locations";
+  } else
+    button.innerHTML = selection;
 }
 
 function search() {
-	var xmlReq = new XMLHttpRequest();
+  var xmlReq = new XMLHttpRequest();
 
-	xmlReq.onload = function() {
-		if (xmlReq.status == 200)
-            createTable(xmlReq.response);
-	}
+  xmlReq.onload = function() {
+    if (xmlReq.status == 200)
+      createTable(xmlReq.response);
+  }
 
-	xmlReq.open('GET', '/search' + getSearchParams(), true);
-	xmlReq.responseType = "json";
-	xmlReq.send(null);
+  xmlReq.open('GET', '/search' + getSearchParams(), true);
+  xmlReq.responseType = "json";
+  xmlReq.send(null);
 }
 
 function getSearchParams() {
-	var category = document.getElementById("categoryButton").innerHTML;
-	var category_id = getIdOfValue(categories, "category", category);
+  var category = document.getElementById("categoryButton").innerHTML;
+  var category_id = getIdOfValue(categories, "category", category);
 
-	var location = document.getElementById("locationButton").innerHTML;
-	var location_id = getIdOfValue(locations, "location", location);
+  var location = document.getElementById("locationButton").innerHTML;
+  var location_id = getIdOfValue(locations, "location", location);
 
-	var user_entry = document.getElementById("searchBox").value;
+  var user_entry = document.getElementById("searchBox").value;
 
-	var requestParam = "?"
-	var firstParam = true;
+  var requestParam = "?"
+  var firstParam = true;
 
-	if(category != "Categories") {
-		requestParam += "category_id=" + category_id;
-		firstParam = false;
-	}
+  if (category != "Categories") {
+    requestParam += "category_id=" + category_id;
+    firstParam = false;
+  }
 
-	if(location != "Locations") {
-		if(!firstParam)
-			requestParam += "&";
+  if (location != "Locations") {
+    if (!firstParam)
+      requestParam += "&";
 
-		requestParam += "location_id=" + location_id;
-		firstParam = false;
-	}
+    requestParam += "location_id=" + location_id;
+    firstParam = false;
+  }
 
-	if(user_entry != "") {
-		if(!firstParam)
-			requestParam += "&";
-		
-		requestParam += "user_entry=" + user_entry;
-	}
+  if (user_entry != "") {
+    if (!firstParam)
+      requestParam += "&";
 
-	return requestParam;
+    requestParam += "user_entry=" + user_entry;
+  }
+
+  return requestParam;
 }
 
 function getIdOfValue(jsonData, field, value) {
-	for(var i = 0; i < jsonData.length; i++) {
-		if(field === "category") {
-			if(jsonData[i].category === value)
-				return jsonData[i].category_id;
-		}
-		else if(field === "location") {
-			if(jsonData[i].location === value)
-				return jsonData[i].location_id;
-		}
-	}
+  for (var i = 0; i < jsonData.length; i++) {
+    if (field === "category") {
+      if (jsonData[i].category === value)
+        return jsonData[i].category_id;
+    } else if (field === "location") {
+      if (jsonData[i].location === value)
+        return jsonData[i].location_id;
+    }
+  }
 }
 
 function getValueOfId(jsonData, field, id) {
-    for(var i = 0; i < jsonData.length; i++) {
-        if(field === "category_id") {
-            if(jsonData[i].category_id === id)
-                return jsonData[i].category;
-        }
-        else if(field === "location_id") {
-            if(jsonData[i].location_id === id)
-                return jsonData[i].location;
-        }
+  for (var i = 0; i < jsonData.length; i++) {
+    if (field === "category_id") {
+      if (jsonData[i].category_id === id)
+        return jsonData[i].category;
+    } else if (field === "location_id") {
+      if (jsonData[i].location_id === id)
+        return jsonData[i].location;
     }
+  }
 }
 
 function createTable(searchResults) {
-    var table = document.createElement("table");
+  var table = document.createElement("table");
 
-    for(var i = 0; i < Math.ceil(searchResults.length); i++)
-    {
-        var row = table.insertRow(-1);
+  for (var i = 0; i < Math.ceil(searchResults.length); i++) {
+    var row = table.insertRow(-1);
 
-        var cell = row.insertCell(-1);
+    var cell = row.insertCell(-1);
 
-                var report = searchResults[i];
-                var category = getValueOfId(categories, "category_id", report.category_id);
-                var image = "report_images/" + report.report_id + ".jpg";
+    var report = searchResults[i];
+    var category = getValueOfId(categories, "category_id", report.category_id);
+    var image = "report_images/" + report.report_id + ".jpg";
 
-                if(locations[parseInt(report.location_id) - 1] == null)
-                	var location = "";
-                else
-                    var location = getValueOfId(locations, "location_id", report.location_id);
+    if (locations[parseInt(report.location_id) - 1] == null)
+      var location = "";
+    else
+      var location = getValueOfId(locations, "location_id", report.location_id);
 
-                cell.innerHTML = "<div onclick=\"viewReports('" + report.report_id + "')\" class='card'>" +
-                                  "<img class='cardImage' src='" + image + "'><div>" +
-                                  category + "</div><div>" +
-                                  report.details + "</div><div>" +
-                                  location + "</div><div>" +
-                                  report.insert_date + "</div><div>" +
-                                  report.status + "</div></div>";
-    }
+    cell.innerHTML = "<div onclick=\"viewReports('" + report.report_id + "')\" class='card'>" +
+      "<img class='cardImage' src='" + image + "'><div>" +
+      category + "</div><div>" +
+      report.details + "</div><div>" +
+      location + "</div><div>" +
+      report.insert_date + "</div><div>" +
+      report.status + "</div></div>";
 
-    var tableContainer = document.getElementById("table");
-    tableContainer.innerHTML = "";
-    tableContainer.appendChild(table);
+    // Add marker for map using this result.
+    // addReportMarkerToMap(reportHazardType, reportThumbnail, reportDetails, reportID, markerLat, markerLng)
+    addReportMarkerToMap(category, image, report.details, report.report_id, report.loc_lat, report.loc_long);
+  }
+
+  var tableContainer = document.getElementById("table");
+  tableContainer.innerHTML = "";
+  tableContainer.appendChild(table);
 }
 
 function viewReports(report_id) {
-    window.location.href = "/reportDetails?report_id=" + report_id;
+  window.location.href = "/reportDetails?report_id=" + report_id;
+}
+
+/*
+Written by: Evan Guan
+Course: CSc 648 Software Engineering Summer 2019 Team 2
+*/
+
+// Includes casting to enforce typing, just in case.
+function setNewCenterLatLng(newLatLng) {
+  curLatLng.lat = Number(newLatLng.lat);
+  curLatLng.lng = Number(newLatLng.lng);
+}
+
+/* Function locates the map div and fills it with the map. */
+function initMap() {
+  /* Function calls to get the lat/lng of the place we are interested in. SF by default. */
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: curLatLng,
+    zoom: 12,
+    mapTypeId: 'hybrid'
+  });
+
+  setNewCenterLatLng({
+    lat: 37.720993,
+    lng: -122.475373
+  });
+}
+
+/*
+Adds a marker to the map, given coordinates or place name.
+Modify the parameters to send this function data to use:
+function addReportMarkerToMap(reportHazardType, reportThumbnail, reportDetails, reportID, markerLat, markerLng, markerIconURL)
+*/
+function addReportMarkerToMap(reportHazardType, reportThumbnail, reportDetails, reportID, markerLat, markerLng) {
+  // This data will be collected from the json returned by a DB query.
+  var infoWindowTitle; // The title of the report from the DB query json.
+  var infoWindowReportHazardType; // The type of hazard of the report from the DB query json.
+  var infoWindowReportThumbnail; // The thumbnail image of the report from the DB query json.
+  var infoWindowReportDetails; // The brief/truncated summary of the report from the DB query json.
+  var infoWindowReportURL; // The full url of the report.
+  // Likely will just be the report id appended to boilerplate url.
+  var newMarkerCoords = {
+    lat: 37.720993,
+    lng: -122.475373
+  }; // The hash {lat: float, lng:float} for the coordinates. See var curLatLng above for formatting.
+  var newMarkerIconURL; // The url of the image to use for the marker icon.
+  // Icons from Google: https://sites.google.com/site/gmapsdevelopment/
+
+  var infoWindowContentString; // The string of html that will be in the popup for the marker on-click.
+
+  // Hard coded example.
+
+  /*
+  infoWindowTitle = 'Generic Title Here';
+  infoWindowReportHazardType = 'Oil Hazard Alert';
+  infoWindowReportThumbnail = 'images/example-infowindow-thumbnail.png';
+  infoWindowReportDetails = 'Large oil slick on intersection of Holloway and 19th avenue.';
+  infoWindowReportURL = 'https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple';
+  markerCoords = curLatLng;
+  markerIconURL = 'http://maps.google.com/mapfiles/ms/micons/caution.png';
+  //*/
+
+  //console.log("Report ID: " + reportID + " " + markerLat + ", " + markerLng);
+  // Dynamic code
+  //*
+  // infoWindowTitle = reportTitle;
+  infoWindowReportHazardType = reportHazardType + ' Alert';
+  infoWindowReportThumbnail = reportThumbnail;
+  infoWindowReportDetails = reportDetails;
+  infoWindowReportURL = '/reports?report_id=' + reportID;
+  newMarkerCoords = (markerLat != null && markerLng != null) ? {lat: markerLat, lng: markerLng} : {lat: 37.7749, lng: -122.4194}
+  newMarkerIconURL = 'http://maps.google.com/mapfiles/ms/micons/caution.png';
+  //*/
+
+  //console.log("Report ID: " + reportID + " " + newMarkerCoords);
+
+  // Create the content string. It is essentially pure html.
+  infoWindowContentString =
+    '<div id="content">' +
+    '<div id="bodyContent">' +
+    '<p>' +
+    '<b>' + infoWindowReportHazardType + '</b><br>' +
+    infoWindowReportDetails + '<br>' +
+    '</p>' +
+    '<p>' +
+    '<a href="' + infoWindowReportURL + '">' +
+    '<img src="' + infoWindowReportThumbnail + '" alt="Click for full report"' +
+    ' style="float:middle; width:96px; height:96px;" align="middle">' +
+    '</a><br>' +
+    '</p>' +
+    '<p>' +
+    '<a ' + 'href="' + infoWindowReportURL + '">' +
+    '<b>Click here for full report.</b>' +
+    '</a> ' +
+    '</p>' +
+    '</div>' +
+    '</div>';
+
+  // Create a new infowindow object for this marker.
+  var infowindow = new google.maps.InfoWindow({
+    content: infoWindowContentString
+  });
+
+  // Now, create the new marker.
+  var newMarker = new google.maps.Marker({
+    position: newMarkerCoords,
+    icon: newMarkerIconURL,
+    title: infoWindowReportHazardType,
+    map: map
+  });
+
+  //* Close infowindow behavior.
+  google.maps.event.addListener(infowindow, 'closeclick', function() {
+    map.setZoom(12);
+  });
+  //*/
+
+  // Click marker behavior.
+  newMarker.addListener('click', function() {
+    infowindow.open(this.map, newMarker);
+    map.panTo(newMarker.position);
+    map.setZoom(18);
+  });
+
+  markers.push(newMarker);
 }
