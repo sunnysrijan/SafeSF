feb6172ca8c086c5b70eade116e3abd6c89250a9
AlexWolski <alex.theinfinite@gmail.com>
2019-08-11 23:54:52 -0700

    Made page dependency imports faster and fixed auth

--- application/view/js/navbar.js
+++ application/view/js/navbar.js
@@ -5,7 +5,10 @@ function loadNavbar () {
     if (this.readyState!==4)
       return
 
+  	//Load the navbar
     document.getElementById('navbar-placeholder').innerHTML= this.responseText;
+    //Check if the user is logged in or not
+    requestAccess();
   }
   
   xmlReq.open('GET', 'navbar-template.html', true);
