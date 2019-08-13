cabc8b02dba5ad60534c5351d15d7480533ec74a
hsaini0914 <you@example.com>
2019-08-13 10:46:49 -0700

    Fixed folder issue

--- application/application/controller/images.js
+++
@@ -1,9 +0,0 @@
-
-exports.getImage = function (data, callback) {
-  if (data['report_id']) {
-    var image_path = './view/hazard_images/' + data['report_id'] + '-thumb.jpg'
-    callback(null, image_path)
-  } else {
-    callback('No ID given', null)
-  }
-}
--- application/application/view/js/captcha.js
+++
@@ -1,5 +0,0 @@
-function onReCAPTCHASuccess () {
-  var submitButton = document.getElementById('submit-button').disabled = false
-  console.log(submitButton)
-  console.log(grecaptcha.getResponse())
-}
