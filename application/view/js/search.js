f2fb47a427a0fd205362d6a2d0e1319704aab167
AlexWolski <alex.theinfinite@gmail.com>
2019-08-14 00:04:20 -0700

    Added text to homepage

--- application/view/js/search.js
+++ application/view/js/search.js
@@ -125,6 +125,28 @@ function getValueOfId (jsonData, field, id) {
   }
 }
 
+//Copied from https://stackoverflow.com/questions/4222690/sorting-a-json-object-in-javascript
+function sortJsonArrayByProperty(objArray, prop, direction){
+    if (arguments.length<2) throw new Error("sortJsonArrayByProp requires 2 arguments");
+    var direct = arguments.length>2 ? arguments[2] : 1; //Default to ascending
+
+    if (objArray && objArray.constructor===Array){
+        var propPath = (prop.constructor===Array) ? prop : prop.split(".");
+        objArray.sort(function(a,b){
+            for (var p in propPath){
+                if (a[propPath[p]] && b[propPath[p]]){
+                    a = a[propPath[p]];
+                    b = b[propPath[p]];
+                }
+            }
+            // convert numeric strings to integers
+            a = a.match(/^\d+$/) ? +a : a;
+            b = b.match(/^\d+$/) ? +b : b;
+            return ( (a < b) ? -1*direct : ((a > b) ? 1*direct : 0) );
+        });
+    }
+}
+
 function createTable (searchResults) {
   var table = document.createElement('table')
 
@@ -161,6 +183,9 @@ function createRecentsTable (searchResults) {
   var table = document.createElement('table')
   var numReports = 10
   var max
+
+  //
+  //sortJsonArrayByProperty(searchResults, 'attributes.insert_date', -1)
 
   for (var i = 0; i < numReports; i++) {
     var row = table.insertRow(-1)
