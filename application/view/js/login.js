3dd7c78c8d3ab92587d3061e52d5ccf3de0f4748
Alex Wolski <awolski@mail.sfsu.edu>
2019-08-14 17:33:40 -0700

    Added error handling to login and registration pages

--- application/view/js/login.js
+++ application/view/js/login.js
@@ -19,7 +19,7 @@ console.log('login clicked')
   }
 
   if (canSubmit) {
-    form.submit()
+    login()
   }
 })
 
@@ -50,30 +50,20 @@ function isPasswordValid (newPassword) {
   }
 }
 
-// function login() {
-//   var username = document.getElementById("username").value
-//   var password = document.getElementById("psw").value
-//   var remember = document.getElementById("remember").checked
-//
-//   if(username === "")
-//     alert("Please enter a username")
-//   else if(password === "")
-//     alert("Please enter a password")
-//
-//   if(username === "" || password === "")
-//     return
-//
-//   var xmlReq = new XMLHttpRequest();
-//
-//   xmlReq.onload = function() {
-//     if (xmlReq.status == 200)
-//       window.location.href =  "/"
-//     else
-//       alert(xmlReq.response)
-//   }
-//
-//   var params = '?username=' + username + '&password=' + password + '&remember=' + remember
-//
-//   xmlReq.open('GET', '/requestLogin' + params, true)
-//   xmlReq.send(null)
-// }
+function login() {
+  var remember = document.getElementById("remember").checked
+
+  var xmlReq = new XMLHttpRequest();
+
+  xmlReq.onload = function() {
+    if (xmlReq.status == 200)
+      window.location.href =  "/"
+    else
+      alert(xmlReq.response)
+  }
+
+  var params = '?username=' + name.value + '&password=' + password.value + '&remember=' + remember
+
+  xmlReq.open('GET', '/requestLogin' + params, true)
+  xmlReq.send(null)
+}
--- application/view/js/registration.js
+++ application/view/js/registration.js
@@ -26,7 +26,7 @@ submitButton.addEventListener('click', function (event) {
   }
 
   if (canSubmit) {
-    form.submit()
+    register()
   }
 })
 
@@ -88,40 +88,18 @@ function isRepeatPasswordValid (newPassword, passwordConfirmation) {
   }
 }
 
-// function register() {
-//   var username = document.getElementById("username").value
-//   var email = document.getElementById("email").value
-//   var password = document.getElementById("psw").value
-//   var passwordConfirmation = document.getElementById("psw-repeat").value
-//   var agree = document.getElementById("invalidCheck").value
-//
-//   if(username === "")
-//     alert("Please enter a username")
-//   else if(email === "")
-//     alert("Please enter an email")
-//   else if(password === "")
-//     alert("Please enter a password")
-//   else if(passwordConfirmation === "")
-//     alert("Please repeat your password")
-//   else if(password != passwordConfirmation)
-//     alert("The passwords do not match. Please re-enter your password")
-//   else if(agree === "")
-//     alert("You must agree the term")
-//
-//   if(username === "" || email === "" || password === "" || passwordConfirmation === "" || password != passwordConfirmation || agree === "" )
-//     return
-//
-//   var xmlReq = new XMLHttpRequest();
-//
-//   xmlReq.onload = function() {
-//     if (xmlReq.status == 200)
-//       window.location.href =  "/"
-//     else
-//       alert(xmlReq.response)
-//   }
-//
-//   var params = '?username=' + username + '&email=' + email + '&password=' + password
-//
-//   xmlReq.open('POST', '/requestRegister' + params, true)
-//   xmlReq.send(null)
-// }
+function register() {
+  var xmlReq = new XMLHttpRequest();
+
+  xmlReq.onload = function() {
+    if (xmlReq.status == 200)
+      window.location.href =  "/"
+    else
+      alert(xmlReq.response)
+  }
+
+  var params = '?username=' + name.value + '&email=' + email.value + '&password=' + password.value
+
+  xmlReq.open('POST', '/requestRegister' + params, true)
+  xmlReq.send(null)
+}
